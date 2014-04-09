40cf4de1096b9f48fd7f648f9341e71f73555480
Charlie Austin <charlie@loyal3.com>
2014-04-09 06:02:50 -0700

    Adding in parsing for doubles

--- src/code_puzzle/repository.clj
+++ src/code_puzzle/repository.clj
@@ -1,7 +1,16 @@
 (ns code-puzzle.repository)
 
-(defn break-into-lines [string]
-  (re-seq (re-pattern ".*\n") string))
+(defn create-pattern [pattern]
+  (re-pattern pattern))
+
+(defn break-into-lines [pattern, string]
+  (re-seq pattern string))
+
+(defn break-into-elem [pattern, string]
+  (re-seq pattern string))
+
+(defn parse-doubles [strings]
+  (map #(Double/parseDouble %1) strings))
 
 (defn get-csv []
   (slurp "resources/runa_data.csv"))
--- test/code_puzzle/repository_test.clj
+++ test/code_puzzle/repository_test.clj
@@ -3,9 +3,22 @@
         code-puzzle.repository))
 
 (def file-input "h1,h2,h3\n1,2,3\n4,5,6\n7,8,9")
+(def double-seq (seq ["1.2" "1" "-3.2"]))
+(def new-line-pattern (re-pattern ".*\n"))
+(def elem-pattern (re-pattern "\\w+|\\d+,"))
 
 
 (deftest repository-test
   (testing "break into lines"
-    (let [actual (break-into-lines file-input)]
+    (let [actual (break-into-lines new-line-pattern file-input)]
       (is (= (first actual) "h1,h2,h3\n")))))
+
+(deftest repository-test-1
+  (testing "break into individual elements"
+    (is (= (break-into-elem elem-pattern "h1,h2,h3\n") (seq ["h1" "h2" "h3"])))
+    (is (= (break-into-elem elem-pattern "1,2,3\n") (seq ["1" "2" "3"])))
+    (is (= (break-into-elem elem-pattern "7,8,9") (seq ["7" "8" "9"])))))
+
+(deftest repository-test-2
+  (testing "parse doubles"
+    (is (= (parse-doubles double-seq) (seq [1.2 1.0 -3.2])))))
